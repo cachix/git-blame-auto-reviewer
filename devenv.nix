6b248dccad6a65a2540c9ed5519b63fd5e50a672
Sander <hey@sandydoo.me>
2025-07-31 13:05:06 +0200

    ci: update dependencies
    Use the same node as github actions does.

--- devenv.nix
+++ devenv.nix
@@ -1,7 +1,11 @@
+{ pkgs, ... }:
+
 {
   languages = {
     typescript.enable = true;
     javascript.enable = true;
+    # Sync with the node version used by Github Actions
+    javascript.package = pkgs.nodejs_20;
     javascript.npm = {
       enable = true;
       install.enable = true;
